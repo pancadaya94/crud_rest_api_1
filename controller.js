1e3b1b2cd8e31c21b31747df96d20b1cc3f87347
unknown <pancadaya94@gmail.com>
2020-10-15 10:08:13 +0700

    selesai membuat crud

--- controller.js
+++ controller.js
@@ -1,3 +1,22 @@
+'use strict';
+
+var response = require('./res');
+var connection = require('./conn');
+
+exports.users = function(req, res) {
+    connection.query('SELECT * FROM person', function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok(rows, res)
+        }
+    });
+};
+
+exports.index = function(req, res) {
+    response.ok("Hello from the Node JS RESTful side!", res)
+};
+
 exports.findUsers = function(req, res) {
     
     var user_id = req.params.user_id;
