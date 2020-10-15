9f2f4d94836cfaa6b866cbaf8423d14d625e1650
unknown <pancadaya94@gmail.com>
2020-10-15 10:29:04 +0700

    complete created crud rest api

--- controller.js
+++ controller.js
@@ -21,7 +21,7 @@ exports.findUsers = function(req, res) {
     
     var user_id = req.params.user_id;
 
-    connection.query('SELECT * FROM person where id = ?',
+    connection.query('SELECT * FROM person where id=?',
     [ user_id ], 
     function (error, rows, fields){
         if(error){
@@ -54,7 +54,7 @@ exports.updateUsers = function(req, res) {
     var first_name = req.body.first_name;
     var last_name = req.body.last_name;
 
-    connection.query('UPDATE person SET first_name = ?, last_name = ? WHERE id = ?',
+    connection.query('UPDATE person SET first_name=?, last_name=? WHERE id=?',
     [ first_name, last_name, user_id ], 
     function (error, rows, fields){
         if(error){
@@ -69,7 +69,7 @@ exports.deleteUsers = function(req, res) {
     
     var user_id = req.body.user_id;
 
-    connection.query('DELETE FROM person WHERE id = ?',
+    connection.query('DELETE FROM person WHERE id=?',
     [ user_id ], 
     function (error, rows, fields){
         if(error){
