79f26f7db061fa85bc14a5fa12918d8f2dc3f810
unknown <pancadaya94@gmail.com>
2020-10-15 09:44:36 +0700

    change port to 8888 filename

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 8888,
     bodyParser = require('body-parser'),
     controller = require('./controller');
 
